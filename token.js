624f54bc43e9c1d621f6da5d3490f56923460914
zeimsim4-ai <zeimsim4@gmail.com>
2025-09-21 06:31:25 +0700

    Update token.js

--- token.js
+++ token.js
@@ -1,5 +1,3 @@
-{
-  "tokens": [
-    "8475111383:AAGI3A7jvG8snX0KEWiubTvfT1ppOOMHHWg"
-  ]
-}
+module.exports = {
+  BOT_TOKEN: "8475111383:AAGI3A7jvG8snX0KEWiubTvfT1ppOOMHHWg", // isi token bot lu
+};
