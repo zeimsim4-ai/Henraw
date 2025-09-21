0e871fb13a3d5cac10fc4ff832b70c34c463aa46
zeimsim4-ai <zeimsim4@gmail.com>
2025-09-21 19:34:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-module.exports = {
-  BOT_TOKEN: "8475111383:AAGI3A7jvG8snX0KEWiubTvfT1ppOOMHHWg", // isi token bot lu
-};
+{
+  "tokens": [
+    "8475111383:AAGI3A7jvG8snX0KEWiubTvfT1ppOOMHHWg"
+  ]
+}
